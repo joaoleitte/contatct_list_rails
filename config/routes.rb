5427c3c9eb7b72b00e92de4e3e02a83e16e63a59
Joao Bezerra <joao.bezerra30@hotmail.com>
2020-12-29 17:02:28 -0300

    Autenticando o usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
   
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+  delete 'sair', to: 'sessions#destroy'
 
   resources :contacts
-  resources :users, onlye: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
 end
